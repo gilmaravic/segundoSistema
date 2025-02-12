cdace9794a68895ce4506cb7782f3cf70e93dd06
gilmara.cvicentini <gilmara.cvicentini@BOT00DN0BFW11-1>
2025-02-12 09:39:28 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace segundoSistema
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string texto = "O Empreenda Senac é uma grande \"Oportunidade\" de adquirir conhecimento .";
+            Console.WriteLine(texto);
+
+
+
+
+
+        }
+    }
+}
